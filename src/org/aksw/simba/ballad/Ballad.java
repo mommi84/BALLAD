365e8d6e78429ec61c3bf6789e194df8137e70a5
mommi84 <mommi84@gmail.com>
2013-11-28 15:59:36 +0100

    Eclipse project

---
+++ src/org/aksw/simba/ballad/Ballad.java
@@ -0,0 +1,18 @@
+package org.aksw.simba.ballad;
+
+/**
+ * BALLAD - Batch Learners Evaluation for Link Discovery.
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ *
+ */
+public class Ballad {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
